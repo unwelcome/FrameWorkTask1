c780a65c7aaa403eb2b9245a63cfdcf15887eb08
unwelcome <moyapohtaxd@mail.ru>
2025-09-16 15:13:38 +0300

    recreated frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,14 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <header>
+    <div class="wrapper">
+      <p>Hello world!</p>
+    </div>
+  </header>
 
-<style scoped></style>
+  <RouterView />
+</template>
+<script lang="ts">
+export default {
+  
+}
+</script>

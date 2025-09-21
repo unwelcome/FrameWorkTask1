4dfe7f9d7a80379d6a3ad69c11f77ef6197cac53
unwelcome <moyapohtaxd@mail.ru>
2025-09-21 16:37:44 +0300

    added search filter to employees page

--- frontend/src/pages/MainPage/Pages/EmployeesPage/EmployeesPage.vue
+++ frontend/src/pages/MainPage/Pages/EmployeesPage/EmployeesPage.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex flex-col grow items-stretch">
     <header class="flex flex-row h-20 items-center gap-5 shrink-0">
-      <searchBar class="w-[380px]" :placeholder="'Поиск по ФИО'"/>
+      <searchBar class="w-[380px]" :placeholder="'Поиск по ФИО'" @change:input="(text: string) => employeeQuery = text"/>
       <textButton class="btn-main text-lg h-10 px-6" :text="'Добавить сотрудника'"/>
     </header>
     <main class="flex flex-col items-stretch bg-bg-2 grow rounded-tl-xl overflow-hidden pt-5 pl-5">
-      <div class="employees-container scrollable pr-5">
-        <div v-for="i of 100" class="employee-item">
-          <employeeCard :show-btns="i % 2 == 0" :redirect-to-profile="true"/>
+      <div class="flex flex-col justify-start items-stretch">
+        <div class="employees-container scrollable pr-5 grow">
+          <div v-for="employee in employeesFilteredList" :key="employee.id" class="employee-item h-[300px] max-w-[250px]">
+            <employeeCard :employee-data="employee" :show-btns="employee.id % 2 == 0" :redirect-to-profile="true"/>
+          </div>
         </div>
       </div>
     </main>
@@ -18,6 +20,48 @@ import employeeCard from './widgets/employeeCard.vue';
 export default {
   components: {
     employeeCard,
+  },
+  data(){
+    return {
+      employeeQuery: '',
+
+      employeesList: [
+        {id: 1, first_name: 'Имя', second_name: 'Фамилия1', third_name: 'Отчество'},
+        {id: 2, first_name: 'Имя', second_name: 'Фамилия2', third_name: 'Отчество'},
+        {id: 3, first_name: 'Имя', second_name: 'Фамилия2', third_name: 'Отчество'},
+        {id: 4, first_name: 'Имя', second_name: 'Фамилия2', third_name: 'Отчество'},
+        {id: 5, first_name: 'Имя', second_name: 'Фамилия2', third_name: 'Отчество'},
+        {id: 6, first_name: 'Имя', second_name: 'Фамилия3', third_name: 'Отчество'},
+        {id: 7, first_name: 'Имя', second_name: 'Фамилия4', third_name: 'Отчество'},
+        {id: 8, first_name: 'Имя', second_name: 'Фамилия5', third_name: 'Отчество'},
+        {id: 9, first_name: 'Имя', second_name: 'Фамилия6', third_name: 'Отчество'},
+        {id: 10, first_name: 'Имя', second_name: 'Фамилия7', third_name: 'Отчество'},
+        {id: 11, first_name: 'Имя', second_name: 'Фамилия8', third_name: 'Отчество'},
+        {id: 12, first_name: 'Имя', second_name: 'Фамилия9', third_name: 'Отчество'},
+        {id: 13, first_name: 'Имя', second_name: 'Фамилия10', third_name: 'Отчество'},
+        {id: 14, first_name: 'Имя', second_name: 'Фамилия11', third_name: 'Отчество'},
+        {id: 15, first_name: 'Имя', second_name: 'Фамилия12', third_name: 'Отчество'},
+        {id: 16, first_name: 'Имя', second_name: 'Фамилия13', third_name: 'Отчество'},
+        {id: 17, first_name: 'Имя', second_name: 'Фамилия14', third_name: 'Отчество'},
+        {id: 18, first_name: 'Имя', second_name: 'Фамилия15', third_name: 'Отчество'},
+        {id: 19, first_name: 'Имя', second_name: 'Фамилия16', third_name: 'Отчество'},
+      ],
+
+      employeesFilteredList: [] as {id: number, first_name: string, second_name: string, third_name: string}[],
+    }
+  },
+  watch: {
+    employeeQuery: {
+      handler(newValue: string) {
+        if (newValue === '') this.employeesFilteredList = this.employeesList;
+        else {
+          const newValueLower = newValue.toLowerCase();
+
+          this.employeesFilteredList = this.employeesList.filter(item => `${item.second_name} ${item.second_name} ${item.third_name}`.toLowerCase().startsWith(newValueLower));
+        }
+      },
+      immediate: true
+    }
   }
 
 }
@@ -27,9 +71,6 @@ export default {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     gap: 20px;
-  }
-
-  .employee-item {
-    min-width: 0; /* Важно для предотвращения переполнения */
+    /* justify-items: start; */
   }
 </style>
--- frontend/src/pages/MainPage/Pages/EmployeesPage/widgets/employeeCard.vue
+++ frontend/src/pages/MainPage/Pages/EmployeesPage/widgets/employeeCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-bg-3 rounded-xl p-2 flex flex-col items-center gap-2 h-full justify-start">
-    <userAvatar class="text-4xl p-4 cursor-pointer" :id="2" :first-name="'Имя'" :second-name="'Фамилия'" @click="redirectToUserProfile"/>
+    <userAvatar class="text-4xl p-4 cursor-pointer" :id="employeeData.id" :first-name="employeeData.first_name" :second-name="employeeData.second_name" @click="redirectToUserProfile"/>
     
     <div class="flex flex-col gap-2 w-full px-2 cursor-default">
       <div class="flex flex-col">
-        <p>Фамилия</p>
-        <p>Имя</p>
-        <p>Отчетсво</p>
+        <p>{{ employeeData.second_name }}</p>
+        <p>{{ employeeData.first_name }}</p>
+        <p>{{ employeeData.third_name }}</p>
       </div>
       <div class="flex flex-col">
         <p>Роль: <span>инженер</span></p>
@@ -26,8 +26,14 @@
   </div>
 </template>
 <script lang="ts">
+import type { PropType } from 'vue';
+
 export default {
   props: {
+    employeeData: {
+      type: Object as PropType<{id: number, first_name: string, second_name: string, third_name: string}>,
+      required: true,
+    },
     showBtns: {
       type: Boolean,
       required: false,

f677b47217a5b976ce37ae8b5732667b9a55b560
unwelcome <moyapohtaxd@mail.ru>
2026-05-03 14:17:21 +0300

    merge fix

--- v1/application/internal/database/postgres/migrator.go
+++ v1/application/internal/database/postgres/migrator.go
@@ -57,7 +57,7 @@ func (m *Migrator) Migrate() {
 		`CREATE INDEX idx_applications_company_status ON applications(company_uuid, status) WHERE deleted_at IS NULL;
 		CREATE INDEX idx_applications_created_by ON applications(created_by) WHERE deleted_at IS NULL;
 		CREATE INDEX idx_applications_managed_by ON applications(managed_by) WHERE deleted_at IS NULL;
-		CREATE INDEX idx_aplicationss_executed_by ON applications(executed_by) WHERE deleted_at IS NULL;
+		CREATE INDEX idx_applications_executed_by ON applications(executed_by) WHERE deleted_at IS NULL;
 		`,
 
 		`CREATE TABLE IF NOT EXISTS application_fix_logs (
